b42fe381cce0a47029c8cbaf5fa29fcf0fb97b24
Krzysztof Retelewski <zebek911@o2.pl>
2022-04-25 13:43:50 +0200

    Opis klasy TextComponent

--- game/include/GameObject/Components/TextComponent.h
+++ game/include/GameObject/Components/TextComponent.h
@@ -11,35 +11,56 @@
 
 class SceneNode;
 
-/** @brief Komponent reprezentujacy model.
+/** @brief Komponent reprezentujacy tekst w swiecie.
  */
 class TextComponent :public Component
 {
-private:
-    unsigned int VAO; //!< Vertex Array Object.
-    unsigned int VBO; //!< Vertex Buffer Object.
-
-    std::string text;
-    Font* font; //!< Wskazanie na siatke, z ktorej korzysta model.
-    
+public:
+    glm::vec3 color = {1.0f, 1.0f, 1.0f}; //!< Kolor tekstu
+    bool alwaysSeen = false; //!< Flaga, czy widac przez sciany
 
 public:
 
     TextComponent();
-
-
     ~TextComponent();
 
-
+    /**
+     * @brief Tworzy komponent tekstu, generuje VBO i VBA
+     * 
+     * @param text Tekst do wyswietlenia
+     * @param font Wskazanie na zasob czcionki
+     * @return true Powodzenie
+     * @return false Niepowodzenie
+     */
     bool Create(const std::string& text, Font* font);
 
-
+    /**
+     * @brief Czysci bufory
+     * 
+     */
     void Clear();
 
-    void FaceCamera(std::shared_ptr<CameraComponent>, SceneNode*);
+    /**
+     * @brief Obraca tekst w strone kamery
+     * 
+     * @param cam Kamera docelowa
+     * @param textNode Wezel zawierajacy komponent tekstu
+     */
+    void FaceCamera(std::shared_ptr<CameraComponent> cam, SceneNode* textNode);
+
+    /**
+     * @brief Wyswietla tekst w swiecie
+     * 
+     * @param shader Shader do renderu tekstu
+     * @return true Powodzenie
+     * @return false Niepowodzenie
+     */
     bool Draw(std::shared_ptr<ShaderComponent> shader);
 
+private:
+    unsigned int VAO; //!< Vertex Array Object.
+    unsigned int VBO; //!< Vertex Buffer Object.
 
-    glm::vec3 color = {1.0f, 1.0f, 1.0f};
-    bool alwaysSeen = false;
+    std::string text; //!< Tekst do wyswietlenia
+    Font* font; //!< Wskazanie na czcionke
 };
